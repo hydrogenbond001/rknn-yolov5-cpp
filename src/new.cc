01001c12a79054c4ae98c5a0d6011912507e9aa1
hydrogenbond001 <119727587+hydrogenbond001@users.noreply.github.com>
2024-12-28 15:24:46 +0800

    1

--- src/new.cc
+++ src/new.cc
@@ -166,7 +166,9 @@ int main(int argc, char **argv)
     const float box_conf_threshold = BOX_THRESH;
     struct timeval start_time, stop_time;
 
-    char *model_name = "./model/RK3588/yolov5s.rknn";
+    char *model_name = (char *)argv[1];
+    char *video_name = (char *)argv[2];
+    //char *model_name = "./model/RK3588/yolov5s.rknn";
     const char* p="hello";
 
     printf("Loading model...\n");
@@ -236,7 +238,7 @@ int main(int argc, char **argv)
 
     //const char* video_path = "/home/orangepi/rknn-cpp-Multithreading-main/jntm.mp4";
     //cv::VideoCapture cap("/home/orangepi/rknn-cpp-Multithreading-main/jntm.mp4");
-    cv::VideoCapture cap(0);
+    cv::VideoCapture cap(video_name);
     //cv::VideoCapture cap("/home/orangepi/rknn-cpp-Multithreading-main/");
     if (!cap.isOpened())
     {
